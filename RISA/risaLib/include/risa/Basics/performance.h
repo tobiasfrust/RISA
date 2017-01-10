bd13059a6eb88d15a5482eba8601904c0b52754e
Tobias Frust <t.frust@hzdr.de>
2017-01-10 10:53:20 +0100

    use of std::chrono::steady_clock

--- RISA/risaLib/include/risa/Basics/performance.h
+++ RISA/risaLib/include/risa/Basics/performance.h
@@ -54,11 +54,11 @@ public:
     * @return  the elapsed time duration between #start() and #stop()
     */
    double elapsed() const {
-      return std::chrono::duration_cast < second_ > (end_ - beg_).count();
+      return std::chrono::duration_cast <second_> (end_ - beg_).count();
    }
 
 private:
-   typedef std::chrono::high_resolution_clock clock_;
+   typedef std::chrono::steady_clock clock_;
    typedef std::chrono::duration<double, std::ratio<1> > second_;
    std::chrono::time_point<clock_> beg_;  //!<  stores the beginning point
    std::chrono::time_point<clock_> end_;  //!<  stores the end time point
